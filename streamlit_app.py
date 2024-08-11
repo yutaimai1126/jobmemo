0110466d661b7cb51067e550b19485bb3c31e465
yutaimai1126 <shitei.shintaichi@gmail.com>
2024-08-11 16:01:40 +0900

    20240811

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
+st.set_page_config(page_title="メインページ", page_icon='icon.png')
+st.title("Multiple OSS Access Log Analyzer")
+
 st.radio("好きなマイケルは？", ('ジャクソン', 'ジョーダン', 'ホフマン'))

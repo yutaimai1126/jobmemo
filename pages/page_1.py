1c06c3ea0f860cf37652ec7040cf69870a9db9f8
yutaimai1126 <shitei.shintaichi@gmail.com>
2024-08-12 02:23:08 +0900

    20240812

--- pages/page_1.py
+++ pages/page_1.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import sqlite3
+import pandas as pd
+from wordcloud import WordCloud
+from matplotlib import pyplot as plt
 
-st.write("Page A")
+with open("comment.txt", "r", encoding="utf-8")as f:
+    text=f.read()
+
+if not text.strip():
+    st.error("テキストが空です。")
+else:
+    wordcloud = WordCloud(background_color="white").generate(text)
+
+plt.imshow(wordcloud)
--- streamlit_app.py
+++ streamlit_app.py
@@ -78,16 +78,3 @@ if submit_button:
     
     cur.close()
     conn.close()
-
-    from wordcloud import WordCloud
-    from matplotlib import pyplot as plt
-
-    with open("comment.txt", "r", encoding="utf-8")as f:
-        text=f.read()
-
-    if not text.strip():
-        st.error("テキストが空です。")
-    else:
-        wordcloud = WordCloud(background_color="white").generate(text)
-
-    plt.imshow(wordcloud)

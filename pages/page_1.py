c9a30b96600fddce16792065c9a8425c56849c51
yutaimai1126 <shitei.shintaichi@gmail.com>
2024-08-12 03:38:30 +0900

    20240812

--- pages/page_1.py
+++ pages/page_1.py
@@ -2,69 +2,60 @@ import streamlit as st
 import pandas as pd
 from matplotlib import pyplot as plt
 import re
+import unicodedata
+from janome.tokenizer import Tokenizer
+from wordcloud import WordCloud
 
+# テキストファイルを読み込み、不要な文字や記号を削除
 with open('comment.txt', encoding='utf-8') as f:
     text = f.read().replace('\n', '').replace(' ', '')
-print(text)
 
-#変数text中に格納されたアンケート内の不要な文字や記号を削除する。
-#re.subで正規表現を使った文字列の削除。
 text = re.sub('\u3000', '', text)
 text = re.sub('・', '', text)
 text = re.sub('「', '', text)
 text = re.sub('」', '', text)
 text = re.sub('（', '', text)
 text = re.sub('）', '', text)
-#re.subで正規表現を使った文字列の半角空白への置換。
 text = re.sub('\n', ' ', text)
 text = re.sub('\\n', '', text)
 text = re.sub('\\n', ' ', text)
 
-#ユニコード正規化
-import unicodedata
-
+# Unicode正規化
 text_sample = unicodedata.normalize('NFKC', text)
 print('UNICODEの正規化後：{}'.format(text_sample))
 
 # janomeを使った形態素解析
-from janome.tokenizer import Tokenizer
-
-#対象のテキストをtokenizeする
 t = Tokenizer()
 tokenized_text = t.tokenize(text)
 
-words_list=[]
-#tokenizeされたテキストをfor文を使ってhinshiとhinshi2に格納する。
+words_list = []
 for token in tokenized_text:
     tokenized_word = token.surface
     hinshi = token.part_of_speech.split(',')[0]
     hinshi2 = token.part_of_speech.split(',')[1]
-    #抜き出す品詞を指定する
     if hinshi == "名詞":
         if (hinshi2 != "数") and (hinshi2 != "代名詞") and (hinshi2 != "非自立"):
             words_list.append(tokenized_word)
 
-words_wakachi=" ".join(words_list)
+words_wakachi = " ".join(words_list)
 print(words_wakachi)
 
-# ワードクラウドによる頻出単語の可視化
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-
-#日本語のフォントを指定する
+# WordCloudの生成と表示
 font = 'ipag.ttf'
+stopWords = ['ので', 'そう', 'から', 'ため', '自分', '投資', '運用']
 
-#意味なさそうな単語（ストップワード）を除去する。
-stopWords = ['ので','そう','から','ため','自分', '投資','運用']
-
-#WordCloudを表示
+# WordCloudを作成
 word_cloud = WordCloud(font_path=font, width=1500, height=900,
-                        stopwords=set(stopWords),min_font_size=5, 
-                        collocations=False, background_color='white', 
-                        max_words=400).generate(words_wakachi)
-figure = plt.figure(figsize=(15,10))
-plt.imshow(word_cloud)
-plt.tick_params(labelbottom=False, labelleft=False)
-plt.xticks([])
-plt.yticks([])
-st.pyplot()
+                      stopwords=set(stopWords), min_font_size=5,
+                      collocations=False, background_color='white',
+                      max_words=400).generate(words_wakachi)
+
+# 新しいfigureとaxesを作成
+fig, ax = plt.subplots(figsize=(15, 10))
+ax.imshow(word_cloud, interpolation='bilinear')
+ax.tick_params(labelbottom=False, labelleft=False)
+ax.set_xticks([])
+ax.set_yticks([])
+
+# Streamlitでの表示
+st.pyplot(fig)

6deb7ebea14df5a3fb1b7392c537cc4827bdcdd3
yutaimai1126 <shitei.shintaichi@gmail.com>
2024-08-11 23:16:49 +0900

    20240811

--- read_DB_from_pandas.py
+++ read_DB_from_pandas.py
@@ -4,6 +4,25 @@ import pandas as pd
 
 st.set_page_config(page_title="Job Memo", page_icon='icon.png')
 
+@st.cache_data  # キャッシュをクリアする
+def init_db():
+    conn = sqlite3.connect('interest.db')
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS sample (
+        company_name TEXT PRIMARY KEY,
+        働き方 INTEGER,
+        給与 INTEGER,
+        福利厚生 INTEGER,
+        やりがい INTEGER,
+        企業理念 INTEGER
+    )
+    ''')
+    conn.commit()
+    conn.close()
+
+init_db()
+
 name = 'タクヤ'
 st.title(f"{name}さんのマイページ")
 
@@ -19,22 +38,9 @@ company_name = st.text_input('説明会を受けた会社名は何ですか？',
 conn = sqlite3.connect('interest.db')
 cur = conn.cursor()
 
-# テーブルが存在しない場合に作成
-cur.execute('''
-CREATE TABLE IF NOT EXISTS sample (
-    company_name TEXT PRIMARY KEY,
-    働き方 INTEGER,
-    給与 INTEGER,
-    福利厚生 INTEGER,
-    やりがい INTEGER,
-    企業理念 INTEGER
-)
-''')
-
 interest_list = ['働き方', '給与', '福利厚生', 'やりがい', '企業理念']
 
 if first_time == 'はい':
-    # 新しいデータフレームを作成
     df = pd.DataFrame(0, index=[company_name], columns=interest_list)
 else:
     df = pd.read_sql('SELECT * FROM sample', conn, index_col='company_name')
@@ -47,15 +53,12 @@ selected_interest = st.radio(
 
 df.loc[company_name, selected_interest] = 1  # Boolean代わりに1を使用
 
-# データフレームの表示
 st.write(df)
 
-# 保存ボタンを追加
 if st.button('保存'):
     df.to_sql('sample', conn, if_exists='replace', index=True)
-    conn.commit()  # 変更をコミット
+    conn.commit()
     st.success("データが保存されました。")
 
-# データベースをクローズする
 cur.close()
 conn.close()

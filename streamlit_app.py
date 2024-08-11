bc7e4b97fb761a6289b327b4bafe38da6721b872
yutaimai1126 <shitei.shintaichi@gmail.com>
2024-08-11 22:55:39 +0900

    20240811

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import sqlite3
+import pandas as pd
 
 st.set_page_config(page_title="Job Memo", page_icon='icon.png')
 
 name = 'タクヤ'
-
 st.title(f"{name}さんのマイページ")
 
 first_time = st.radio(
@@ -12,56 +12,25 @@ first_time = st.radio(
     ['はい','いいえ']
     )
 
-if first_time == 'はい':
-    st.header(f'{name}さん、会社説明会お疲れさまでした')
+st.header(f'{name}さん、会社説明会お疲れさまでした')
+company_name = st.text_input('説明会を受けた会社名は何ですか？', '')
 
-    company_name = st.text_input('会社名', '')
-
-    ziku = st.radio(
-        f'{name}さんは就活の軸が決まっていますか？',
-        ['決まっている','決まっていない']
-        )
-
-    if ziku == '決まっている':
-        st.subheader(f'{company_name}')
-        st.text(f'{name}さんは何に興味を持ちましたか？')
-    elif ziku == '決まっていない':
-        st.subheader(f'{company_name}')
-        st.text(f'{name}さんの就活の軸は何ですか？')
-    
-    interest_list = ['働き方','給与','福利厚生','やりがい','企業理念']
-    for interest in interest_list:
-        st.checkbox(interest)
-    
-
-
-# データベースに接続する
+# データベースに接続
 conn = sqlite3.connect('interest.db')
-c = conn.cursor()
+cur = conn.cursor()
+if first_time == 'はい':
+    # データフレーム読み込み
+    interest_list = ['働き方','給与','福利厚生','やりがい','企業理念']
+    df = pd.DataFrame(index=company_name, columns=interest_list)
+else:
+    df = pd.read_sql('SELECT * FROM sample', conn)
 
-def show_data():
-    c.execute('SELECT * FROM users')
-    data = c.fetchall()
-    for d in data:
-        st.write(d)
+st.text(f'{name}さんが最も興味を持ったことは何ですか？')
+for i, interest in enumerate(interest_list):
+    df.loc[company_name,interest] = st.radio(interest)
 
-# データを追加する
-def add_data(name, age):
-    c.execute('INSERT INTO users (name, age) VALUES (?, ?)', (name, age))
-    conn.commit()
-    st.write('Data added. Please reload page.')
-
-# データベースにテーブルを作成する
-c.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER)')
-
-# データの表示
-show_data()
-
-# データの追加
-name = st.text_input('Name')
-age = st.number_input('Age')
-if st.button('Add data'):
-    add_data(name, age)
+df.to_sql('sample', conn, if_exists='replace')
 
 # データベースをクローズする
+cur.close()
 conn.close()

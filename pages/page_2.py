1955ceab55a26aa92627d8da36a08331b1b72c1d
yutaimai1126 <shitei.shintaichi@gmail.com>
2024-08-12 08:35:54 +0900

    20240812

--- pages/page_2.py
+++ pages/page_2.py
@@ -1,3 +1,36 @@
 import streamlit as st
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.write("Page B")
+# Streamlitアプリケーションのタイトル
+st.title('各項目の合計')
+
+# データベース接続
+dbname = 'interest.db'
+conn = sqlite3.connect(dbname)
+
+# データを読み込む
+query = 'SELECT * FROM Interest'
+df = pd.read_sql(query, conn)
+
+# 各項目について合計を計算する
+interest_list = ['働き方', '給与', '福利厚生', 'やりがい', '企業理念']
+sum_interests = df[interest_list].sum()
+
+# グラフを作成
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# 各項目の合計を棒グラフで表示
+sum_interests.plot(kind='bar', color='skyblue', ax=ax)
+
+# グラフのタイトルとラベルを設定
+ax.set_title('各項目の合計')
+ax.set_xlabel('Interest')
+ax.set_ylabel('Total Count')
+
+# グラフをStreamlitに表示
+st.pyplot(fig)
+
+# 接続を閉じる
+conn.close()
--- test.py
+++ test.py
@@ -1,70 +1,33 @@
-# MIcrosoft Visual Studio をインストール
-# https://visualstudio.microsoft.com/ja/vs/community/
-
-import re
-
-with open('comment.txt', encoding='utf-8') as f:
-    text = f.read().replace('\n', '').replace(' ', '')
-print(text)
-
-#変数text中に格納されたアンケート内の不要な文字や記号を削除する。
-#re.subで正規表現を使った文字列の削除。
-text = re.sub('\u3000', '', text)
-text = re.sub('・', '', text)
-text = re.sub('「', '', text)
-text = re.sub('」', '', text)
-text = re.sub('（', '', text)
-text = re.sub('）', '', text)
-#re.subで正規表現を使った文字列の半角空白への置換。
-text = re.sub('\n', ' ', text)
-text = re.sub('\\n', '', text)
-text = re.sub('\\n', ' ', text)
-
-#ユニコード正規化
-import unicodedata
-
-text_sample = unicodedata.normalize('NFKC', text)
-print('UNICODEの正規化後：{}'.format(text_sample))
-
-# janomeを使った形態素解析
-from janome.tokenizer import Tokenizer
-
-#対象のテキストをtokenizeする
-t = Tokenizer()
-tokenized_text = t.tokenize(text)
-
-words_list=[]
-#tokenizeされたテキストをfor文を使ってhinshiとhinshi2に格納する。
-for token in tokenized_text:
-    tokenized_word = token.surface
-    hinshi = token.part_of_speech.split(',')[0]
-    hinshi2 = token.part_of_speech.split(',')[1]
-    #抜き出す品詞を指定する
-    if hinshi == "名詞":
-        if (hinshi2 != "数") and (hinshi2 != "代名詞") and (hinshi2 != "非自立"):
-            words_list.append(tokenized_word)
-
-words_wakachi=" ".join(words_list)
-print(words_wakachi)
-
-# ワードクラウドによる頻出単語の可視化
-from wordcloud import WordCloud
+import sqlite3
+import pandas as pd
 import matplotlib.pyplot as plt
 
-#日本語のフォントを指定する
-font = 'ipag.ttf'
+# データベース接続
+dbname = 'interest.db'
+conn = sqlite3.connect(dbname)
 
-#意味なさそうな単語（ストップワード）を除去する。
-stopWords = ['ので','そう','から','ため','自分', '投資','運用']
+# データを読み込む
+query = 'SELECT * FROM Interest'
+df = pd.read_sql(query, conn)
 
-#WordCloudを表示
-word_cloud = WordCloud(font_path=font, width=1500, height=900,
-                        stopwords=set(stopWords),min_font_size=5, 
-                        collocations=False, background_color='white', 
-                        max_words=400).generate(words_wakachi)
-figure = plt.figure(figsize=(15,10))
-plt.imshow(word_cloud)
-plt.tick_params(labelbottom=False, labelleft=False)
-plt.xticks([])
-plt.yticks([])
+# 各項目について合計を計算する
+interest_list = ['働き方', '給与', '福利厚生', 'やりがい', '企業理念']
+sum_interests = df[interest_list].sum()
+
+# グラフを作成
+plt.figure(figsize=(10, 6))
+
+# 各項目の合計を棒グラフで表示
+sum_interests.plot(kind='bar', color='skyblue')
+
+# グラフのタイトルとラベルを設定
+plt.title('各項目の合計')
+plt.xlabel('Interest')
+plt.ylabel('Total Count')
+
+# グラフを表示
+plt.tight_layout()
 plt.show()
+
+# 接続を閉じる
+conn.close()

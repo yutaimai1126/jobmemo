be0057fe4f9d028227a3a2d9f8b4885859b2d99f
yutaimai1126 <shitei.shintaichi@gmail.com>
2024-08-11 16:12:58 +0900

    20240811

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ st.set_page_config(page_title="Job Memo", page_icon='icon.png')
 
 st.title("タクヤさんのマイページ")
 
-st.header('会社説明会')
+st.header('おかえりなさい')
 
 company_name = st.text_input('会社名', '')
 
-st.subheader(f'{company_name}')
+st.subheader(f'{company_name}会社説明会')
 st.text('タクヤさんは何に興味を持ちましたか？')
 
 interest_list = ['国内シェア','海外進出度','将来性']

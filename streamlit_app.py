e02c16f5a1f65e8ea8ee3f11bc317bd0d56144d6
yutaimai1126 <shitei.shintaichi@gmail.com>
2024-08-11 22:58:06 +0900

    20240811

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ st.title(f"{name}さんのマイページ")
 
 first_time = st.radio(
     f'JobMemoの利用は初めてですか？',
-    ['はい','いいえ']
-    )
+    ['はい', 'いいえ']
+)
 
 st.header(f'{name}さん、会社説明会お疲れさまでした')
 company_name = st.text_input('説明会を受けた会社名は何ですか？', '')
@@ -18,18 +18,22 @@ company_name = st.text_input('説明会を受けた会社名は何ですか？',
 # データベースに接続
 conn = sqlite3.connect('interest.db')
 cur = conn.cursor()
+
+interest_list = ['働き方', '給与', '福利厚生', 'やりがい', '企業理念']
+
 if first_time == 'はい':
-    # データフレーム読み込み
-    interest_list = ['働き方','給与','福利厚生','やりがい','企業理念']
-    df = pd.DataFrame(0,index=company_name, columns=interest_list)
+    # 新しいデータフレームを作成
+    df = pd.DataFrame(index=[company_name], columns=interest_list)
 else:
-    df = pd.read_sql('SELECT * FROM sample', conn)
+    df = pd.read_sql('SELECT * FROM sample', conn, index_col='index')
 
 st.text(f'{name}さんが最も興味を持ったことは何ですか？')
-for i, interest in enumerate(interest_list):
-    df.loc[company_name,interest] = st.radio(interest)
+for interest in interest_list:
+    selected_interest = st.radio(interest, ['1', '2', '3', '4', '5'], index=2)
+    df.loc[company_name, interest] = selected_interest
 
-df.to_sql('sample', conn, if_exists='replace')
+# データを保存
+df.to_sql('sample', conn, if_exists='append', index=True)
 
 # データベースをクローズする
 cur.close()

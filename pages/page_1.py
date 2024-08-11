94d4fa52593bf28476c7f269e5077ceb7b21f3d6
yutaimai1126 <shitei.shintaichi@gmail.com>
2024-08-12 03:34:48 +0900

    20240812

--- pages/page_1.py
+++ pages/page_1.py
@@ -1,32 +1,70 @@
 import streamlit as st
-import sqlite3
 import pandas as pd
-from wordcloud import WordCloud
 from matplotlib import pyplot as plt
-import japanize_matplotlib
+import re
 
+with open('comment.txt', encoding='utf-8') as f:
+    text = f.read().replace('\n', '').replace(' ', '')
+print(text)
 
-# テキストファイルを読み込み
-with open("comment.txt", "r", encoding="utf-8") as f:
-    text = f.read()
+#変数text中に格納されたアンケート内の不要な文字や記号を削除する。
+#re.subで正規表現を使った文字列の削除。
+text = re.sub('\u3000', '', text)
+text = re.sub('・', '', text)
+text = re.sub('「', '', text)
+text = re.sub('」', '', text)
+text = re.sub('（', '', text)
+text = re.sub('）', '', text)
+#re.subで正規表現を使った文字列の半角空白への置換。
+text = re.sub('\n', ' ', text)
+text = re.sub('\\n', '', text)
+text = re.sub('\\n', ' ', text)
 
-# テキストの表示
-st.write(text)
+#ユニコード正規化
+import unicodedata
 
-# テキストが空でないかチェック
-if not text.strip():
-    st.error("テキストが空です。")
-else:
-    # WordCloudを生成
-    wordcloud = WordCloud(background_color="white").generate(text)
+text_sample = unicodedata.normalize('NFKC', text)
+print('UNICODEの正規化後：{}'.format(text_sample))
 
-    # WordCloudを表示
-    plt.figure(figsize=(10, 5))
-    plt.title("WordCloud")
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    
-    # Streamlitでの表示
-    st.pyplot(plt)
-    plt.imshow(wordcloud)
+# janomeを使った形態素解析
+from janome.tokenizer import Tokenizer
 
+#対象のテキストをtokenizeする
+t = Tokenizer()
+tokenized_text = t.tokenize(text)
+
+words_list=[]
+#tokenizeされたテキストをfor文を使ってhinshiとhinshi2に格納する。
+for token in tokenized_text:
+    tokenized_word = token.surface
+    hinshi = token.part_of_speech.split(',')[0]
+    hinshi2 = token.part_of_speech.split(',')[1]
+    #抜き出す品詞を指定する
+    if hinshi == "名詞":
+        if (hinshi2 != "数") and (hinshi2 != "代名詞") and (hinshi2 != "非自立"):
+            words_list.append(tokenized_word)
+
+words_wakachi=" ".join(words_list)
+print(words_wakachi)
+
+# ワードクラウドによる頻出単語の可視化
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+#日本語のフォントを指定する
+font = 'ipag.ttf'
+
+#意味なさそうな単語（ストップワード）を除去する。
+stopWords = ['ので','そう','から','ため','自分', '投資','運用']
+
+#WordCloudを表示
+word_cloud = WordCloud(font_path=font, width=1500, height=900,
+                        stopwords=set(stopWords),min_font_size=5, 
+                        collocations=False, background_color='white', 
+                        max_words=400).generate(words_wakachi)
+figure = plt.figure(figsize=(15,10))
+plt.imshow(word_cloud)
+plt.tick_params(labelbottom=False, labelleft=False)
+plt.xticks([])
+plt.yticks([])
+plt.show()
--- test.py
+++ test.py
@@ -1,29 +1,70 @@
-import os
-import sqlite3
+# MIcrosoft Visual Studio をインストール
+# https://visualstudio.microsoft.com/ja/vs/community/
 
-def check_db_schema():
-    db_path = 'interest.db'
-    
-    if not os.path.exists(db_path):
-        print(f"{db_path} は存在しません。")
-        return
-    
-    with sqlite3.connect(db_path) as conn:
-        cur = conn.cursor()
-        cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-        tables = cur.fetchall()
-        
-        if tables:
-            print("データベース内のテーブル:")
-            for table in tables:
-                print(f"- {table[0]}")
-                
-                cur.execute(f"PRAGMA table_info({table[0]});")
-                columns = cur.fetchall()
-                print("カラム情報:")
-                for column in columns:
-                    print(f"  - {column[1]}: {column[2]}")
-        else:
-            print("データベースにはテーブルがありません。")
+import re
 
-check_db_schema()
+with open('comment.txt', encoding='utf-8') as f:
+    text = f.read().replace('\n', '').replace(' ', '')
+print(text)
+
+#変数text中に格納されたアンケート内の不要な文字や記号を削除する。
+#re.subで正規表現を使った文字列の削除。
+text = re.sub('\u3000', '', text)
+text = re.sub('・', '', text)
+text = re.sub('「', '', text)
+text = re.sub('」', '', text)
+text = re.sub('（', '', text)
+text = re.sub('）', '', text)
+#re.subで正規表現を使った文字列の半角空白への置換。
+text = re.sub('\n', ' ', text)
+text = re.sub('\\n', '', text)
+text = re.sub('\\n', ' ', text)
+
+#ユニコード正規化
+import unicodedata
+
+text_sample = unicodedata.normalize('NFKC', text)
+print('UNICODEの正規化後：{}'.format(text_sample))
+
+# janomeを使った形態素解析
+from janome.tokenizer import Tokenizer
+
+#対象のテキストをtokenizeする
+t = Tokenizer()
+tokenized_text = t.tokenize(text)
+
+words_list=[]
+#tokenizeされたテキストをfor文を使ってhinshiとhinshi2に格納する。
+for token in tokenized_text:
+    tokenized_word = token.surface
+    hinshi = token.part_of_speech.split(',')[0]
+    hinshi2 = token.part_of_speech.split(',')[1]
+    #抜き出す品詞を指定する
+    if hinshi == "名詞":
+        if (hinshi2 != "数") and (hinshi2 != "代名詞") and (hinshi2 != "非自立"):
+            words_list.append(tokenized_word)
+
+words_wakachi=" ".join(words_list)
+print(words_wakachi)
+
+# ワードクラウドによる頻出単語の可視化
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+#日本語のフォントを指定する
+font = 'ipag.ttf'
+
+#意味なさそうな単語（ストップワード）を除去する。
+stopWords = ['ので','そう','から','ため','自分', '投資','運用']
+
+#WordCloudを表示
+word_cloud = WordCloud(font_path=font, width=1500, height=900,
+                        stopwords=set(stopWords),min_font_size=5, 
+                        collocations=False, background_color='white', 
+                        max_words=400).generate(words_wakachi)
+figure = plt.figure(figsize=(15,10))
+plt.imshow(word_cloud)
+plt.tick_params(labelbottom=False, labelleft=False)
+plt.xticks([])
+plt.yticks([])
+plt.show()

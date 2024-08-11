5dc5526ddd83b5cc91b3db0525e0d30f2703e25e
yutaimai1126 <shitei.shintaichi@gmail.com>
2024-08-12 02:26:47 +0900

    20240812

--- pages/page_1.py
+++ pages/page_1.py
@@ -4,15 +4,24 @@ import pandas as pd
 from wordcloud import WordCloud
 from matplotlib import pyplot as plt
 
+# テキストファイルを読み込み
+with open("comment.txt", "r", encoding="utf-8") as f:
+    text = f.read()
 
-with open("comment.txt", "r", encoding="utf-8")as f:
-    text=f.read()
-
+# テキストの表示
 st.write(text)
 
+# テキストが空でないかチェック
 if not text.strip():
     st.error("テキストが空です。")
 else:
+    # WordCloudを生成
     wordcloud = WordCloud(background_color="white").generate(text)
 
-plt.imshow(wordcloud)
+    # WordCloudを表示
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    
+    # Streamlitでの表示
+    st.pyplot(plt)

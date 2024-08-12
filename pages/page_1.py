7e5b43bcd8f934645782657b8be323e47199504a
yutaimai1126 <shitei.shintaichi@gmail.com>
2024-08-12 11:11:31 +0900

    20240812

--- pages/page_1.py
+++ pages/page_1.py
@@ -50,7 +50,7 @@ print(words_wakachi)
 
 # WordCloudの生成と表示
 font = 'ipag.ttf'
-stopWords = ['ので', 'そう', 'から', 'ため', '活', '的', '内', '感', '度', 'タイム', '魅力','非常']
+stopWords = ['ので', 'そう', 'から', 'ため', '活', '的', '内', '感', '度', 'タイム', '魅力','非常','制','方']
 
 # WordCloudを作成
 word_cloud = WordCloud(font_path=font, width=1500, height=900,
@@ -108,7 +108,7 @@ print(words_wakachi)
 
 # WordCloudの生成と表示
 font = 'ipag.ttf'
-stopWords = ['ので', 'そう', 'から', 'ため', '活', '的', '内', '感', '度', 'タイム', '魅力','非常','さ','者','期','強調','可能','性','課題','懸念']
+stopWords = ['ので', 'そう', 'から', 'ため', '活', '的', '内', '感', '度', 'タイム', '魅力','非常','制','方''さ','者','期','強調','可能','性','課題','懸念']
 
 # WordCloudを作成
 word_cloud = WordCloud(font_path=font, width=1500, height=900,
